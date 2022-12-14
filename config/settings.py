7967712b170c16930cb7bd01dc289dee959add4c
Thierry Chappuis <tchappui@gmail.com>
2022-12-14 16:49:14 +0100

    Displayed form using django-widget-tweaks

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "widget_tweaks",
     "wepynaire",
     "wepynaire.pages",
     "wepynaire.persons",
